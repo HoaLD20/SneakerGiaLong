233b91d019d3cee9c63f334aaef84ead8521ce6a
leduchoa911 <leduchoa911@gmail.com>
2022-03-05 13:15:02 +0700

    connect db, login, ...

--- src/main/java/com/sneaker/snakergialong/Controller/User/LoginServlet.java
+++ src/main/java/com/sneaker/snakergialong/Controller/User/LoginServlet.java
@@ -1,5 +1,7 @@
 package com.sneaker.snakergialong.Controller.User;
 
+import com.sneaker.snakergialong.Model.Dao.UserDao;
+import com.sneaker.snakergialong.Model.Entities.User;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -15,6 +17,30 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //get User Name and password when Customer inptut
+        String username = request.getParameter("SigninName");
+        String password = request.getParameter("SigninPassword");
+        UserDao userDao = new UserDao(); // constructor User
+        User user = new User();
+        user = userDao.signIn(username, password); // call funtion to sinup
+        // if null it mean user not have exist and sendirect failed.jsp
+        if (user == null) {
+            //TODO : MAKE forget password
+            request.setAttribute("message", "Cant't Login <br/> Wrong username or password .. ");
+            getServletContext().getRequestDispatcher("/Failed.jsp").forward(request, response);
+        } else {
+            //set session for login user
+            HttpSession session = request.getSession(true);
+            session.setAttribute("LoginUser", user);
+//            session.setMaxInactiveInterval(60*15);
 
+            if (user.getuRole().equalsIgnoreCase("admin")) {
+//                System.out.println("admin dang nhap ne hihi");
+                response.sendRedirect("admin/Index.jsp");   //admin
+            } else {
+//                System.out.println("ko phai admin nha");
+                response.sendRedirect("index.jsp");
+            }
+        }
     }
 }
---
+++ src/main/java/com/sneaker/snakergialong/Model/utilize/SHA.java
@@ -0,0 +1,37 @@
+
+package com.sneaker.snakergialong.Model.utilize;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA {
+
+    /**
+     * encrypt password using SHA-512 Algorithm
+     *
+     * @param passwordToHash
+     * @return encrypted Password
+     */
+    public static String encrypt(String passwordToHash) {
+        String generatedPassword = null;
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(passwordToHash.getBytes());
+            //Get the hash's bytes 
+            byte[] bytes = md.digest();
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for (byte aByte : bytes) {
+                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+}
